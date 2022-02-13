0573f98559582af79d8a8899ce4db5914e8710b9
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-02-14 02:06:09 +0530

    Create Maximum_Subarray.cpp
    https://leetcode.com/problems/maximum-subarray/

---
+++ Array/Maximum_Subarray.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int cursum = 0;
+        int maxsum = nums[0];
+
+
+        //Edge Case: when all elements are negative
+        int count = 0;
+        int maxm = nums[0];
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]<0){
+                count++;
+                maxm = max(nums[i], maxm);
+            }
+            else{
+                break;
+            }
+        }
+
+        if(count==nums.size()){
+            return maxm;
+        }
+
+
+        //When any one element is positive.
+        else{
+
+            for(int i=0; i<nums.size(); i++){
+
+                cursum = cursum + nums[i];
+                if(cursum < 0){
+                    cursum = 0;
+                }
+
+                maxsum = max(cursum, maxsum);
+            }
+
+            return maxsum;
+
+        }
+    }
+};

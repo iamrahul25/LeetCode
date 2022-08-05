6c6dacb75061a07b7ed85f773dbd7214951422df
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-08-05 17:02:16 +0530

    Create arranging-coins.cpp
    https://leetcode.com/problems/arranging-coins/

---
+++ Binary Search/arranging-coins.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        
+        long long low = 1;
+        long long high = n;
+
+        long long mid = (low+high)/2;
+
+        int ans=0;
+
+        while(low<=high){
+
+
+            mid = (low + high) / 2;
+
+            // cout<<"low: "<<low<<" high: "<<high<<" mid: "<<mid<<endl;
+
+            if(((mid*(mid+1))/2)==n){
+                ans = mid;
+                break;
+            }
+
+            else if(((mid*(mid+1))/2) > n){
+                high = mid-1;
+            }
+
+            else if(((mid*(mid+1))/2) < n){
+                low = mid+1;
+                ans = mid;
+            }
+        }
+
+        return ans;
+    }
+};

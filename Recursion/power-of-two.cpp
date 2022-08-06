1143fe4ffe6079fa766858850815c8d29d882c1c
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-08-06 17:24:01 +0530

    Update power-of-two.cpp

--- Recursion/power-of-two.cpp
+++ Recursion/power-of-two.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
-    bool isPowerOfTwo(int n) {
+    bool isPowerOfTwo(long long int n) {
         
-        //Method 1:
+        //Method 1-----------------:
         
         //if(n==0) return false;
         //if(ceil(log2(n)) == floor(log2(n))) return true;
         //return false;
         
         
-        //Method 2:
+        //Method 2-----------------:
+        //if(n==0) return false;
+        
+        // while(n!=1){
+        //     if(n%2!=0){
+        //         return false;
+        //     }
+        //     n = n/2;
+        // }
+        // return true;
+        
+        
+        //Method 3:------------Bit Manipulation.
+        
         if(n==0) return false;
         
-        while(n!=1){
-            if(n%2!=0){
-                return false;
-            }
-            n = n/2;
+        if((n & (n-1))==0){
+            return true;
         }
-        return true;
+        else{
+            return false;
+        }
         
     }
 };

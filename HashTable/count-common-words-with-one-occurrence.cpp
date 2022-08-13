137550c1a741cc5cda55879b477a661fc3307fe7
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-08-14 01:31:49 +0530

    Create count-common-words-with-one-occurrence.cpp
    https://leetcode.com/problems/count-common-words-with-one-occurrence/

---
+++ HashTable/count-common-words-with-one-occurrence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countWords(vector<string>& words1, vector<string>& words2) {
+        unordered_map<string, int> mpp1;
+        unordered_map<string, int> mpp2;
+        
+        for(auto s: words1){
+            mpp1[s]++;
+        }
+        
+        for(auto s: words2){
+            mpp2[s]++;
+        }
+        
+        int count = 0;
+        for(auto x: words1){
+            if(mpp1[x]==1 and mpp2[x]==1){
+                count++;
+            }
+        }
+        
+        return count;
+    }
+};

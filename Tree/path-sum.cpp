7bbd1f6d2ec3d4ed06fbbd02f07d969c6722cf8c
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-08-14 13:07:39 +0530

    Create path-sum.cpp
    https://leetcode.com/problems/path-sum/

---
+++ Tree/path-sum.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool pathsum(TreeNode* root, int sum, int target){
+        if(root==NULL){
+            return false;
+        }
+        
+        if(root->left==NULL and root->right==NULL){
+            sum = sum + root->val;
+            
+            if(sum==target) return true;
+            else return false;
+        }
+        
+        return pathsum(root->left, sum+root->val, target) or pathsum(root->right, sum+root->val, target);
+    }
+    
+    
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        return pathsum(root, 0, targetSum);
+    }
+};

ef09690cb2d1b8d453215d6b047820bb317da5e4
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-08-27 14:57:08 +0530

    Create set-mismatch.cpp
    https://leetcode.com/problems/set-mismatch/

---
+++ Sorting/set-mismatch.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        vector<int> ans(2);
+        
+        int n = nums.size();
+        
+        //Finding Repeating element.
+        for(int i=0; i<n-1; i++){
+            if(nums[i]==nums[i+1]){
+                ans[0] = nums[i];
+                break;
+            }
+        }
+        
+        //Finding Not existing Element
+        int notfound = 1;
+        for(int i=0; i<n; i++){
+            if(nums[i]==notfound){
+                notfound++;
+            }
+        }
+        
+        ans[1] = notfound;
+        
+        return ans;
+    }
+};

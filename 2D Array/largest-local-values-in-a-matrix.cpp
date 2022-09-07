84cbc7beec67a32ffa64a5e4ff9a7e1a18cbf0d8
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-09-07 11:58:08 +0530

    Create largest-local-values-in-a-matrix.cpp
    https://leetcode.com/problems/largest-local-values-in-a-matrix/

---
+++ 2D Array/largest-local-values-in-a-matrix.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    
+    //Not the best solution: We can further optimise this solution. 
+    //Time complexity: 9*(N*N)  
+    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+        int n = grid.size();
+        
+        //Creating (n-2) x (n-2) Matrix
+        vector<vector<int>> matrix(n-2, vector<int>(n-2));
+        
+        for(int i=0; i<n-2; i++){
+            for(int j=0; j<n-2; j++){
+                
+                int maxx = 0;
+                for(int p=i; p<i+3; p++){
+                    for(int q=j; q<j+3; q++){
+                        maxx = max(maxx, grid[p][q]);
+                    }
+                }
+                
+                matrix[i][j] = maxx;
+            }
+        }
+        
+        return matrix;
+    }
+};

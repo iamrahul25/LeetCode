654f1806ac293745436a8ba8c812b3da4c9485bd
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-09-08 11:39:24 +0530

    Update toeplitz-matrix.cpp

--- 2D Array/toeplitz-matrix.cpp
+++ 2D Array/toeplitz-matrix.cpp
@@ -1,44 +1,60 @@
 class Solution {
 public:
     
-    //Time Complexity: O(M*N)  Space: O(1)
-    bool isToeplitzMatrix(vector<vector<int>>& matrix) {
-        
+    //Best Approach. => Short Code.  Time: O(M*N) Space: O(1)
+    bool isToeplitzMatrix(vector<vector<int>>& matrix){
         int r = matrix.size();
         int c = matrix[0].size();
         
-        
-        //Upper Diagonals
-        for(int i=0; i<c; i++){
-            
-            int a = 0, b = i;
-            int last = matrix[a][b];
-            
-            while(a<min(r,c) and b<c){
-                
-                if(matrix[a][b]!=last){
-                    return false;
-                }
-                a++; b++;
-            }
-        }
-        
-        
-        //Left Side Diagonals
         for(int i=1; i<r; i++){
-            
-            int a = i, b = 0;
-            int last = matrix[a][b];
-            
-            while(a<r and b<min(r,c)){
-                if(matrix[a][b]!=last){
+            for(int j=1; j<c; j++){
+                if(matrix[i][j]!=matrix[i-1][j-1]){
                     return false;
                 }
-                
-                a++; b++;
             }
         }
         
         return true;
     }
+    
+    //Time Complexity: O(M*N)  Space: O(1) ---- Over Use of Brain ----Long Method. 
+//     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
+        
+//         int r = matrix.size();
+//         int c = matrix[0].size();
+        
+//         //Upper Diagonals
+//         for(int i=0; i<c; i++){
+            
+//             int a = 0, b = i;
+//             int last = matrix[a][b];
+            
+//             while(a<min(r,c) and b<c){
+                
+//                 if(matrix[a][b]!=last){
+//                     return false;
+//                 }
+//                 a++; b++;
+//             }
+//         }
+        
+        
+//         //Left Side Diagonals
+//         for(int i=1; i<r; i++){
+            
+//             int a = i, b = 0;
+//             int last = matrix[a][b];
+            
+//             while(a<r and b<min(r,c)){
+//                 if(matrix[a][b]!=last){
+//                     return false;
+//                 }
+                
+//                 a++; b++;
+//             }
+//         }
+        
+//         return true;
+//     }
+    
 };

349d586fcf221c05720a0e61fb1d349ce1831864
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-11-02 21:56:21 +0530

    Update best-time-to-buy-and-sell-stock-ii.cpp

--- Greedy/best-time-to-buy-and-sell-stock-ii.cpp
+++ Greedy/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     
-    //Greedy Approach. Time: O(N)  and  Space: O(1)
+    //Greedy Approach. Time: O(N)  and  Space: O(1) Complexity. 
     int maxProfit(vector<int>& prices) {
         
         int res = 0; 
         int n = prices.size();
 
-        
         for(int i=1; i<n; i++){
             res = res + max(prices[i]-prices[i-1], 0);
         }

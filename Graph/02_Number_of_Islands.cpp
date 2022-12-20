d96af1cf9fd5f732e4e06e73fd76d8581d0e1cc4
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-12-20 15:11:40 +0530

    Update 02_Number_of_Islands.cpp

--- Graph/02_Number_of_Islands.cpp
+++ Graph/02_Number_of_Islands.cpp
@@ -1,6 +1,78 @@
 class Solution {
 public:
     
+    //Short Code:-----------------------------------------------------------------------------
+    //Approach 2: DFS Traversal 
+    void dfs(int row, int col, vector<vector<char>>&grid){
+        grid[row][col] = '0';
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int arrx[4] = {-1,+1,0,0};
+        int arry[4] = {0,0,-1,+1};
+
+        for(int k=0; k<4; k++){
+            int x = arrx[k] + row;
+            int y = arry[k] + col;
+
+            if((x>=0 and x<n) and (y>=0 and y<m) and (grid[x][y]=='1')){
+                grid[x][y] = '0';
+                dfs(x,y,grid);
+            }
+        }
+    }
+
+    //Approach 1: BFS Traversal
+    void bfs(int row, int col, vector<vector<char>>&grid){
+        grid[row][col] = '0';
+        queue<pair<int,int>> q;
+        q.push({row, col});
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int arrx[4] = {-1,+1,0,0};
+        int arry[4] = {0,0,-1,+1};
+
+        while(!q.empty()){
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+
+            for(int k=0; k<4; k++){
+                int x = arrx[k] + i;
+                int y = arry[k] + j;
+
+                if((x>=0 and x<n) and (y>=0 and y<m) and (grid[x][y]=='1')){
+                    grid[x][y] = '0';
+                    q.push({x,y});
+                }
+            }
+        }
+    }
+
+    //Main Function
+    int numIslands(vector<vector<char>>& grid){
+        int n = grid.size();
+        int m = grid[0].size();
+        int count = 0;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(grid[i][j]=='1'){
+                    count++;
+                    dfs(i,j,grid); //We can use dfs() or bfs() anyone here! 
+                }
+            }
+        }
+
+        return count;
+    }
+    //Short Code:-----------------------------------end-----------------------------------------
+    
+    
+    //Long Code:-----------------------------------------------------------------------------
     //Approach 1: BFS   Time: O(N^2)  Space: O(N^2)
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size();
@@ -51,4 +123,4 @@ public:
         
         return count;
     }
-};
+    //Long Code:-----------------------------------end------------------------------------------

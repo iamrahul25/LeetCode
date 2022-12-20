9da53aff8574eb2e1e7f331e4fc19eb06fe91f34
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2022-12-20 14:19:31 +0530

    Update 01_Number_of_Provinces.cpp
    https://leetcode.com/problems/number-of-provinces/

--- Graph/01_Number_of_Provinces.cpp
+++ Graph/01_Number_of_Provinces.cpp
@@ -1,15 +1,48 @@
 class Solution {
 public:
     //***In this Problem we have to find number of disconnected graph. ***//
-    //Approach 2: BFS Traversal  (Breadth First Search) 
+    //Approach 2: Directly apply BFS or DFS Traversal to Adjacency Matrix.
+    void bfs(int n, vector<vector<int>>&adjMatrix, vector<int>&visited){
+        queue<int> q;
+        q.push(n);
+        visited[n] = 1;
+        
+        int len = adjMatrix.size();
+        
+        while(!q.empty()){
+            int front = q.front();
+            q.pop();
+            
+            for(int i=0; i<len; i++){
+                if((visited[i]==0) and (adjMatrix[front][i]==1)){
+                    visited[i] = 1;
+                    q.push(i);
+                }
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<int> visited(n, 0);
+        int count = 0;
+        
+        for(int i=0; i<n; i++){
+            if(visited[i]==0){
+                count++;
+
+                bfs(i, isConnected, visited);
+            }
+        }
+        
+        return count;
+    }
     
     
     
-    
-    
-    
-    //Approach 1: DFS Traversal
-    //Converting Adjacency Matrix to Adjacency List. Then DFS Traversal.
+ 
+
+    //Approach 1: Converting Adjacency Matrix to Adjacency List. Then applying BFS or DFS Traversal.
     void dfs(int node, vector<int> adj[], vector<int> &visited){
         visited[node] = 1;
         

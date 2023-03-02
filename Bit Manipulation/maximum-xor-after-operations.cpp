a0c691136bba2165dc50f36115aea3c1b8d0a463
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2023-03-02 22:47:15 +0530

    Update maximum-xor-after-operations.cpp

--- Bit Manipulation/maximum-xor-after-operations.cpp
+++ Bit Manipulation/maximum-xor-after-operations.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
+
+    //Approach 1: Time: O(N) Space: O(32)
+    // int maximumXOR(vector<int>& nums) {
+
+    //     vector<int> arr(32, 0);
+
+    //     for(int i=0; i<nums.size(); i++){
+    //         for(int j=0; j<32; j++){
+    //             arr[j] = arr[j] + ((nums[i]>>j)&1);
+    //         }
+    //     }
+
+    //     int ans = 0;
+    //     for(int i=0; i<32; i++){
+    //         if(arr[i]!=0){
+    //             ans = ans + pow(2, i);
+    //         }
+    //     }
+
+    //     return ans;
+    // }
+
+    //Approach 2: Time: O(N) Space: O(1) Simple and Easy
     int maximumXOR(vector<int>& nums) {
 
-        vector<int> arr(32, 0);
-
-        for(int i=0; i<nums.size(); i++){
-            for(int j=0; j<32; j++){
-                arr[j] = arr[j] + ((nums[i]>>j)&1);
-            }
-        }
-
         int ans = 0;
-        for(int i=0; i<32; i++){
-            if(arr[i]!=0){
-                ans = ans + pow(2, i);
-            }
+        for(int i=0; i<nums.size(); i++){
+            ans = ans | nums[i];
         }
 
         return ans;

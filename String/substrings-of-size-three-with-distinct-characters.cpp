5cc8a0e10af826b7617dc58a6c933a206fdf53dc
Rahul Kumar <76434100+iamrahul25@users.noreply.github.com>
2023-03-06 17:26:22 +0530

    Create substrings-of-size-three-with-distinct-characters.cpp
    https://leetcode.com/problems/substrings-of-size-three-with-distinct-characters/

---
+++ String/substrings-of-size-three-with-distinct-characters.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countGoodSubstrings(string s) {
+        int n = s.size();
+        if(n<3) return 0;
+
+        int count = 0;
+        for(int i=0; i<n-2; i++){
+            if(s[i]!=s[i+1] && s[i+1]!=s[i+2] && s[i]!=s[i+2]) count++;
+        }
+
+        return count;
+    }
+};
